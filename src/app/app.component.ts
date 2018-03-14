a6754b638717e8c3f165972a1e535b999d98c8a3
AlfredoMartinezMartinez <alfredo.martinez1@alu.uclm.es>
2018-03-14 12:13:04 +0100

    aplicacion con google maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MapasService} from './providers/mapas.service';
+import {Marcador} from './interfaces/marcador.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  lat: number = 38.990439;
+  lng: number = -3.919896;
+  zoom:number = 16;
+
+  marcadorSel:any=null;
+  draggable:string="1";
+  constructor(public _ms:MapasService){
+    this._ms.cargarMarcadores();
+
+  }
+  clickMapa(evento){
+    let nuevoMarcador:Marcador={
+      lat:evento.coords.lat,
+      lng:evento.coords.lng,
+      titulo:"",
+      draggable:true
+    }
+    // console.log(evento);
+    this._ms.insertarMarcador(nuevoMarcador);
+  }
+  clickMarcador(marcador:Marcador,i :number){
+    console.log(marcador,i);
+    this.marcadorSel= marcador;
+  }
+
+  dragEndMarcador(marcador:Marcador,evento){
+    console.log(marcador,evento);
+    let lat=evento.coords.lat;
+    let lng= evento.coords.lng;
+    marcador.lat=lat;
+    marcador.lng=lng;
+    this._ms.guardarMarcadores();
+  }
+
+  cambiarDraggable(){
+    if(this.draggable=="1"){
+      this.marcadorSel.draggable= true;
+    }else{
+      this.marcadorSel.draggable=false;
+    }
+
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+//mapas
+import { AgmCoreModule } from '@agm/core';
 
+//providers
+import {MapasService} from './providers/mapas.service';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAotNPNXhjHBSsqntLw1lyJJEiJAaKUkdE'
+    }),
+    FormsModule
   ],
-  providers: [],
+  providers: [MapasService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
